2dd3e5db2bfb5b863f5d11a6e4a9f15acb21ee69
echoflame14 <joshpeterson.dev@gmail.com>
2016-08-17 14:37:36 -0600

    hooked up ui-router and created a view

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -1,1 +1,9 @@
-angular.module("app", []);
+angular.module("app", ["ui.router"])
+.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("/");
+	$stateProvider
+		.state("/home", {
+			url:"/",
+			templateUrl:"html/views/home.html"
+		});
+});
