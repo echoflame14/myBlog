6f715ae507c93fa8c4be453f8a526cfb4214380a
echoflame14 <joshpeterson.dev@gmail.com>
2016-08-17 16:57:33 -0600

    Able to add posts with the service file

--- public/js/angular/ctrl.js
+++ public/js/angular/ctrl.js
@@ -1,3 +1,4 @@
 angular.module("app").controller("ctrl",function($scope,srvc){
 	$scope.test = srvc.test;
+	$scope.posts = srvc.posts;
 });
--- public/js/angular/srvc.js
+++ public/js/angular/srvc.js
@@ -1,3 +1,33 @@
 angular.module("app").service("srvc", function(){
 	this.test = "working";
+	this.getTime = function(){
+		var monthNames = [
+		  "January", "February", "March",
+		  "April", "May", "June", "July",
+		  "August", "September", "October",
+		  "November", "December"
+		];
+		var date = new Date();
+		var day = date.getDate();
+		var monthIndex = date.getMonth();
+		var year = date.getFullYear();
+		var formatedDate = monthNames[monthIndex] + " " + day + ", " + year;
+		return formatedDate;
+	};
+	this.posts = {
+		post1:{
+			title: "LET'S DO THIS!",
+			time: this.getTime(),
+			imgAlt: "coming soon",
+			article: "Today I start my blog. Writing anything other than code feels strange to me.. I've been at DevMountain for 7 weeks now."
+		},
+		post0: {
+			title: "HEY THIS IS A TITLE",
+			time: this.getTime(),
+			imgSrc: "https://static.pexels.com/photos/2752/city-sunny-people-street.jpg",
+			imgAlt: "New York",
+			article: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		}
+
+	};
 });
