bc801edd40f9d8ef4f8e648f7f693fa0ec3e8d95
echoflame14 <joshpeterson.dev@gmail.com>
2016-08-17 14:15:50 -0600

    created angular app and connected to index.html

---
+++ public/js/angular/ctrl.js
@@ -0,0 +1,3 @@
+angular.module("app").controller("ctrl",function($scope,srvc){
+	$scope.test = srvc.test;
+});
---
+++ public/js/angular/srvc.js
@@ -0,0 +1,3 @@
+angular.module("app").service("srvc", function(){
+	this.test = "working";
+});
