abef7fc64b93193e2ce57ed1649a28c8b3d9a4ee
echoflame14 <joshpeterson.dev@gmail.com>
2016-08-18 12:38:36 -0600

    Posts now display properly when inserted into the table

--- public/js/angular/ctrl.js
+++ public/js/angular/ctrl.js
@@ -1,6 +1,9 @@
 angular.module("app").controller("ctrl",function($scope,srvc){
 	$scope.test = srvc.test;
-	$scope.posts = srvc.posts;
+	$scope.getPosts = srvc.posts().then(function(resp){
+		$scope.posts = resp;
+	});
+	$scope.counter = 0;
 	$scope.makePost = function(){
 		alert("hey");
 	};
--- public/js/angular/srvc.js
+++ public/js/angular/srvc.js
@@ -1,4 +1,4 @@
-angular.module("app").service("srvc", function(){
+angular.module("app").service("srvc", function($http){
 	this.test = "working";
 	this.getTime = function(){
 		var monthNames = [
@@ -14,23 +14,12 @@ angular.module("app").service("srvc", function(){
 		var formatedDate = monthNames[monthIndex] + " " + day + ", " + year;
 		return formatedDate;
 	};
-	this.posts = {
-		post1:{
-			title: "LET'S DO THIS!",
-			time: this.getTime(),
-			imgAlt: "coming soon",
-			imgSrc: "img/day-1.jpg",
-			article: "Today I start my blog. Writing anything other than code feels strange to me.. I've been at DevMountain for 7 weeks now."
-		},
-		post0: {
-			title: "HEY THIS IS A TITLE",
-			time: this.getTime(),
-			imgSrc: "https://static.pexels.com/photos/2752/city-sunny-people-street.jpg",
-			imgAlt: "New York",
-			article: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-		}
-
-	};
+	this.posts = function(){
+		return $http.get("/posts").then(function(response){
+			console.log("then in service completed", response);
+			return response.data;
+		});
+	}
 	this.makePost = function(){
 		console.log("make a post bruh");
 	};
