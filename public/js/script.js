ba9b9f29df62bfd0b58425b67503bc3f21cba954
echoflame14 <joshpeterson.dev@gmail.com>
2016-08-23 11:12:13 -0600

    mvp styles for navbar

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,29 @@ var home = $("#home");
 var about = $("#about");
 var proj = $("#proj");
 var cont = $("#contact");
+
+
+home.click(function(){
+	home.addClass("active");
+	about.removeClass("active");
+	proj.removeClass("active");
+	cont.removeClass("active");
+});
+about.click(function(){
+	about.addClass("active");
+	home.removeClass("active");
+	proj.removeClass("active");
+	cont.removeClass("active");
+});
+proj.click(function(){
+	proj.addClass("active");
+	about.removeClass("active");
+	home.removeClass("active");
+	cont.removeClass("active");
+});
+cont.click(function(){
+	cont.addClass("active");
+	about.removeClass("active");
+	proj.removeClass("active");
+	home.removeClass("active");
+});
