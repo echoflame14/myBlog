d9a1ba7d2113cc7847ba94f7a6ebf5ee64aa29f5
echoflame14 <joshpeterson.dev@gmail.com>
2016-09-20 13:35:11 -0600

    working on improvements before pushing to live

--- private/eApp.js
+++ private/eApp.js
@@ -2,18 +2,22 @@ var express = require("express");
 var cors = require("cors");
 var massive = require("massive");
 var bodyParser = require("body-parser");
+var passport = require("passport");
 //end require
 var app = module.exports = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("../public"));
-var port = 80;
-app.listen(port,function(){
-	console.log("listening on port:",port);
+// var port = 80; only for when live
+var port = 3000;
+app.listen(port, function () {
+	console.log("listening on port:", port);
 });
 //end app.use and app.listen config
 var connectionString = "postgress://postgres:Gtars123@localhost/blog"; // "postgess://username/host/databaseName"
-var massiveInstance = massive.connectSync({connectionString : connectionString}); // who knows
+var massiveInstance = massive.connectSync({
+	connectionString: connectionString
+}); // who knows
 app.set("db", massiveInstance); // setting the "db" obj to equal th massive.connectSync
 var db = app.get("db"); // setting var db to app.get "db"
 //end db connection
@@ -22,7 +26,8 @@ app.get("/posts", mainCtrl.getPosts);
 app.put("/secret/:postid", mainCtrl.putPost);
 app.post("/secret/post", mainCtrl.postPost);
 app.delete("/secret/:postid", mainCtrl.deletePost);
-app.get("/secret/mess", mainCtrl.getMessages);
+app.get("/secret/mess", passport.authenticate("local"), function () { // testing passport auth
+});
 app.get("/secret/ratings", mainCtrl.getRatings);
 app.post("/message", mainCtrl.postMess);
 app.post("/secret/postrating", mainCtrl.ratePost);
--- private/mainCtrl.js
+++ private/mainCtrl.js
@@ -1,111 +1,101 @@
-var app = require("./eApp.js");
-var db = app.get("db");
+var app = require( "./eApp.js" );
+var db = app.get( "db" );
 
 module.exports = {
-	getPosts: function(req,res,next){
-		db.getPosts(function(err,resp){
-			if(err){
-				res.send(err);
-			}
-			else{
+	getPosts: function ( req, res, next ) {
+		db.getPosts( function ( err, resp ) {
+			if ( err ) {
+				res.send( err );
+			} else {
 
-				res.send(resp);
+				res.send( resp );
 			}
-		});
+		} );
 	},
-	putPost: function(req,res,next){
-		db.putPost(req.body.posttext, req.body.posttitle, req.body.posttime, req.body.postimgsrc, req.body.postimgalt, req.params.postid, function(err,resp){
-			if(err){
-				res.send(err);
+	putPost: function ( req, res, next ) {
+		db.putPost( req.body.posttext, req.body.posttitle, req.body.posttime, req.body.postimgsrc, req.body.postimgalt, req.params.postid, function ( err, resp ) {
+			if ( err ) {
+				res.send( err );
+			} else {
+				res.send( resp );
 			}
-			else{
-				res.send(resp);
-			}
-		});
+		} );
 	},
-	postPost: function(req,res,next){
-		db.postPosts([req.body.posttext, req.body.posttitle, req.body.posttime, req.body.postimgsrc, req.body.postimgalt], function(err,resp){
-			if(err){
-				res.send(err);
+	postPost: function ( req, res, next ) {
+		db.postPosts( [ req.body.posttext, req.body.posttitle, req.body.posttime, req.body.postimgsrc, req.body.postimgalt ], function ( err, resp ) {
+			if ( err ) {
+				res.send( err );
+			} else {
+				res.send( resp );
 			}
-			else{
-				res.send(resp);
-			}
-		});
+		} );
 	},
-	deletePost: function(req,res,next){
-		db.deletePost([req.params.postid], function(err,resp){
-			if(err){
-				console.log(err);
-				res.send(err);
-			}
-			else{
+	deletePost: function ( req, res, next ) {
+		db.deletePost( [ req.params.postid ], function ( err, resp ) {
+			if ( err ) {
+				console.log( err );
+				res.send( err );
+			} else {
 
-				res.send(resp);
+				res.send( resp );
 			}
-		});
+		} );
 	},
-	postMess: function(req,res,next){
-		var squirtle = [req.body.messname,req.body.messemail, req.body.messmess];
+	postMess: function ( req, res, next ) {
+		var squirtle = [ req.body.messname, req.body.messemail, req.body.messmess ];
 		// var squirtle = {
 		// 	messname: req.body.name,
 		// 	messemail: req.body.email,
 		// 	messmess: req.body.message
 		// };
-		console.log("running postMess squirtle now =", squirtle);
-		db.postMess(squirtle, function(err, resp){
-			if(err){
-				console.log("error in the mainCtrl.js");
+		console.log( "running postMess squirtle now =", squirtle );
+		db.postMess( squirtle, function ( err, resp ) {
+			if ( err ) {
+				console.log( "error in the mainCtrl.js" );
+
+			} else {
+				console.log( "got to the mainCtrl in the app" );
 
 			}
-			else{
-				console.log("got to the mainCtrl in the app");
-
+		} );
+		console.log( "running selectMessName squirtle =", squirtle );
+		db.selectMess( squirtle[ 0 ], function ( err, resp ) {
+			if ( err ) {
+				console.log( err );
+				res.send( err );
+			} else {
+				console.log( resp );
+				res.send( resp );
 			}
-		});
-		console.log("running selectMessName squirtle =", squirtle);
-		db.selectMess(squirtle[0],function(err, resp){
-			if(err){
-				console.log(err);
-				res.send(err);
-			}
-			else{
-				console.log(resp);
-				res.send(resp);
-			}
-		}
-	);
+		} );
 	},
 
-	getMessages: function(req,res,next){
-		db.getMessages(function(err,resp){
-			if(err){
-				res.send(err);
+	getMessages: function ( req, res, next ) {
+		db.getMessages( function ( err, resp ) {
+			if ( err ) {
+				res.send( err );
+			} else {
+				res.send( resp );
 			}
-			else{
-				res.send(resp);
-			}
-		});
+		} );
 	},
-	getRatings: function(req,res,next){
-		db.getRatings(function(err,resp){
-			if(err){
-				res.send(err);
+	getRatings: function ( req, res, next ) {
+		db.getRatings( function ( err, resp ) {
+			if ( err ) {
+				res.send( err );
+			} else {
+				res.send( resp );
 			}
-			else{
-				res.send(resp);
-			}
-		});
+		} );
 	},
-	ratePost: function(req,res,next){
-		db.ratePost(req.body.postid,req.body.rating, function(err,resp){
-			if(err){
-				res.send(err);
+	ratePost: function ( req, res, next ) {
+		db.ratePost( req.body.postid, req.body.rating, function ( err, resp ) {
+			if ( err ) {
+				res.send( err );
+			} else {
+				res.send( resp );
 			}
-			else{
-				res.send(resp);
-			}
-		});
+		} );
 	}
 
 };
